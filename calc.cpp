5e6223fc31e8fc31db4c2d690f107ccc3a1ac307
mrunals01 <mrunalsambrekar.01@gmail.com>
2015-08-17 11:21:03 +0530

    Update calc.cpp
    Updating in the other branch

--- calc.cpp
+++ calc.cpp
@@ -1,11 +1,12 @@
 /* This is to create a sample code for calculator.
   File is created using gitHub */
 
-// Hello, I am Mrunal. 
 // This is new version of my file 
 
 #include <iostream.h>
 int main()
 {
   cout<<"This is Calculator Application";
+  cout<<"This file is updated in this branch";
+  cout<<"Welcome to gitHub platform";
 }
