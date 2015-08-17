f10645feea9e3a812f310f97a6ab14ade57edfb3
mrunals01 <mrunalsambrekar.01@gmail.com>
2015-08-17 11:07:12 +0530

    Update calc.cpp
    main() added

--- calc.cpp
+++ calc.cpp
@@ -1,2 +1,11 @@
 /* This is to create a sample code for calculator.
   File is created using gitHub */
+
+// Hello, I am Mrunal. 
+// This is new version of my file 
+
+#include <iostream.h>
+int main()
+{
+  cout<<"This is Calculator Application";
+}
